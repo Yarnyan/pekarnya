885ffabfb35730f6396281a214679a0509e9cc3f
Hopt Dev <hoptdev@outlook.com>
2024-10-21 01:35:41 +0500

    1

---
+++ backend/pp2/Migrations/20241020192417_ttt2.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pp2.Migrations
+{
+    /// <inheritdoc />
+    public partial class ttt2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleId",
+                table: "Users",
+                newName: "BakeryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                newName: "IX_Users_BakeryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PIN",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Hidden",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Roles",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Roles_Users_UserId",
+                table: "Roles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Bakeries_BakeryId",
+                table: "Users",
+                column: "BakeryId",
+                principalTable: "Bakeries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Roles_Users_UserId",
+                table: "Roles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Bakeries_BakeryId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Hidden",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Roles");
+
+            migrationBuilder.RenameColumn(
+                name: "BakeryId",
+                table: "Users",
+                newName: "RoleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_BakeryId",
+                table: "Users",
+                newName: "IX_Users_RoleId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PIN",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
