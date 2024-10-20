d6f172ca84eaa510e603eded483e8ee6ceea4175
Hopt Dev <hoptdev@outlook.com>
2024-10-21 03:21:35 +0500

    1

--- frontend/src/module/task/modal/CreateTaskModal.tsx
+++ frontend/src/module/task/modal/CreateTaskModal.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {useState} from 'react' 
 import { forwardRef } from 'react';
 import { Box } from '@mui/material';
@@ -48,4 +49,56 @@ const CreateTaskModal = forwardRef<HTMLDivElement, Props>(({ onClose }, ref) => 
   );
 });
 
+=======
+import {useState} from 'react' 
+import { forwardRef } from 'react';
+import { Box } from '@mui/material';
+import { styleModal } from '../../../components/modal/modal';
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import CloseIcon from '@mui/icons-material/Close';
+type Props = {
+  onClose: () => void;
+};
+
+type Inputs = {
+  login: string;
+  password: string;
+};
+
+const CreateTaskModal = forwardRef<HTMLDivElement, Props>(({ onClose }, ref) => {
+
+  const schema = yup.object().shape({
+    login: yup.string().required("Login is required"),
+    password: yup.string().required("Password is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
+
+
+  return (
+    <Box ref={ref} sx={{ ...styleModal }} tabIndex={0}>
+        <div>
+            <div className='w-full justify-end flex'>
+                <button onClick={onClose}>
+                    <CloseIcon />
+                </button>
+            </div>
+            <div className='w-full'>
+                <p className="text-center text-[20px] font-medium text-[var(--textBarColor)]">Новая задача</p>
+            </div>
+        </div>
+    </Box>
+  );
+});
+
+>>>>>>> 2e27dee245f6962f7d020a277ea5b15c12f592ec
 export default CreateTaskModal;
